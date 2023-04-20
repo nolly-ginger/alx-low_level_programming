fb45e64510eb11b9bb39c5288e8f9d4d1775551c
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-20 12:01:24 -0700

    Print value of different variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main-print size of variouscomputer files
+ * Return:Always o(Success)
+ */
+
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 8 byte(s)");
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	return (0);
+}
