23f1efbf4f6fa3ffcb00b38d656f4feadda3566d
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-20 11:35:21 -0700

    Print exact sentance in printf fun

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,12 @@
+#include <stcio.h>
+
+/**
+ * main-print exact sentance in printf
+ * Return:Always 0(Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
