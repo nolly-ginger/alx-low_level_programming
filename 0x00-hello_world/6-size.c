076222283c95e1a6d70fc5075b7e37f187ba8384
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-20 12:46:33 -0700

    Assemble CFILE and save in main.s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	puts("Size of a char: 1 byte(s)");
-	puts("Size of an int: 4 byte(s)");
-	puts("Size of a long int: 4 byte(s)");
-	puts("Size of a long long int: 8 byte(s)");
-	puts("Size of a float: 4 byte(s)");
+	puts("Size of a char: %v byte(s)", sizeof(char));
+	puts("Size of an int: %v byte(s)", sizeof(int));
+	puts("Size of a long int: %v byte(s)", sizeof(long int));
+	puts("Size of a long long int: %v byte(s)", sizeof(long long int));
+	puts("Size of a float: %v byte(s)", sizeof(float));
 	return (0);
 }
