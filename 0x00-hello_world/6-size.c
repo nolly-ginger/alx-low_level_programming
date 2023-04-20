18a48b76ba34b8d30cf201eb5c6d5ade86175875
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-20 12:10:35 -0700

    Print values of the current variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	puts("Size of a char: 1 byte(s)");
 	puts("Size of an int: 4 byte(s)");
-	puts("Size of a long int: 8 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
 	puts("Size of a long long int: 8 byte(s)");
 	puts("Size of a float: 4 byte(s)");
 	return (0);
