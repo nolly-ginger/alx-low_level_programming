a42d0622c94e41305106b79e733c09c7faf9f588
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-20 09:50:31 -0700

    Print out the following sent

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+/**
+ *print out sentence
+ */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n")
-	
-	Return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	Return(0);
 }
