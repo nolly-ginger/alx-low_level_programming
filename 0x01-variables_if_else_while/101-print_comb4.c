651b61e8660e7a76f2c28b1afadf6465ddb353a9
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-21 14:49:24 -0700

    print combination of 3 different numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination for three digets
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int n, m, s;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (s = 50; s < 58; s++)
+			{
+				for (s > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(s);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
