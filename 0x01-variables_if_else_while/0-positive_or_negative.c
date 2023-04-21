a942138c389824453f67c810b2e45634d4d45803
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-21 10:05:47 -0700

    Create program that prints whether n is positive/negative/=0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entry poing
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	
-	if(n > 0)
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n < 0)
+	else if (n == 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is zero\n, n");
 	}
-	else(n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	return(0);
+	else
+		printf("%d is negative\n, n");
+	return (0);
 }
