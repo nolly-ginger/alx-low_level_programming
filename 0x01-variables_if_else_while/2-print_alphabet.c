de77753175fde22f47b7803f10d5fcf7e8f5b9e5
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-21 12:43:53 -0700

    Print alphabets with lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * creating list of alphabets
+ *
+ * return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(abcdefghijklmnopqrstuvwxyz);
+	}
+
+	putchar("\n");
+	return (0);
+}
