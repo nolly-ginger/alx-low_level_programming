f32ffdb6d6e35f4747cceb7401722e28887d8f81
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-21 08:58:04 -0700

    Create program printing whether n is negative/positive/=0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int n;
+	
+	if(n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if(n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else(n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	return(0);
+}
