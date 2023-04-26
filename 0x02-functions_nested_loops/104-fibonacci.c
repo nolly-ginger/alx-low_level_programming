f271f5ed4ff61474b62ae46baee124ec22ea4de5
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-26 14:52:35 -0700

    Print first 98 numbers of Fobinacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 	n1 = 1;
 	n2 = 2;
 	boolean1 = boolean2 = 1;
-	printf("%ld, %ld", n1, n2);
-	for (i = 0; i < 96; i++)
+	printf("%lu, %lu", n1, n2);
+	for (i = 2; i < 98; i++)
 	{
 		if (boolean1)
 		{
 			fn1 = n1 + n2;
-			printf(", %ld", fn1);
+			printf(", %lu", fn1);
 			n1 = n2;
 			n2 = fn1;
 		}
@@ -36,8 +36,8 @@ int main(void)
 			}
 			fn2 = (n11 + n22);
 			fn1 = n1 + n2 + (fn2 / 1000000000);
-			printf(", %ld", fn1);
-			printf("%ld", fn2 % 1000000000);
+			printf(", %lu", fn1);
+			printf("%lu", fn2 % 1000000000);
 			n1 = n2;
 			n11 = n22;
 			n2 = fn1;
