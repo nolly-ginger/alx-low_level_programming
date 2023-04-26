7963035adb66df46ad923f057158ed5eb9802561
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-25 19:54:48 -0700

    Print timetable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 				{
 					_putchar(((c / 100) % 10) + 48);
 					_putchar(((c / 10) % 10) + 48);
-					_putchar((c / 10) + 48);
+					_putchar((c % 10) + 48);
 				}
 			}
 			_putchar('\n');
