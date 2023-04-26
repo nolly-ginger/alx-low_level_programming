bb2cc8027cc56b5910d523c5e6dd14cc1d89ef5c
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-25 19:00:15 -0700

    Add two integers and print result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - ad two integers and print reults
+ * @x: represents first integer
+ * @y: represents second integer
+ *
+ * Return: total of two integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
