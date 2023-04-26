efefd30029e1051164c02b5160d45f3f21d8e079
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-25 20:03:41 -0700

    Print sum of multiples as directed

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print multiples of 3 and 5 under 1024
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 1; x < 1024; x++)
+	{
+		if ((x % 3) == 0 && (x % 5) == 0)
+		{
+			y += a;
+		}
+	}
+	printf("%d\n", y);
+	return (0);
+}
