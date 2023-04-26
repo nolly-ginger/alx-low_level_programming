06ac8c8ebacd6f944b3f9721922b7120ae44c07c
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-25 17:55:44 -0700

    Compute all input interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - compute the absolute value of an integar
+ * @i: character displaying the integers
+ *
+ * Return: Alesyd (Success)
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
