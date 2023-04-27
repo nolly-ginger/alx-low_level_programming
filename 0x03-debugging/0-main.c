4a63004ee93eae7a57ba4cc33b67629612946c5f
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-27 03:16:44 -0700

    Creating a file testing for positive or negative integers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,17 +2,14 @@
 
 /**
  * main - used to test whether integer is positive or negative
- * gives corrupt for value=0
- *
  * Return: Always 0(Success)
  */
 
 int main(void)
 {
 	int i;
-	int != 0;
-
-	i = 45;
+	
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
