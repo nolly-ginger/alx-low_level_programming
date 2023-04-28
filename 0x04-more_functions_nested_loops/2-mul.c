25dd8c862b652be504cc2c7fb0066102c5d9aa0d
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-28 06:17:46 -0700

    Multiply twi integers and give output

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - input for two integers being multiplied
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: output of multipliccation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
