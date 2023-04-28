eec87a1e9418156bd47f394065c71cf8c3fdba6d
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-28 04:32:56 -0700

    Print return=1 for uppercase and return=0 for lowercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _isupper - will be the entry point of code
+ * to check for all uppercase charactrs
+ * @c: will displace the input character
+ *
+ * Return: 1 if c is uppercase, otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	c = 28;
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		while (c > 'a')
+			c++;
+		{
+			return (0);
+		}
+		return (1);
+	}
+	return (1);
+}
