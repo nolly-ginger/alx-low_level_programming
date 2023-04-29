47338daae625e28db194be7c44f3b6c4e0deebb4
Noluthando <lebohangmashiqa7@gmail.com>
2023-04-29 05:27:01 -0700

    Print digits from 1-100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Print number from 1-100 followed by new line
+ * which are numbers of 3 and 5
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i<= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && 1 % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+			
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
