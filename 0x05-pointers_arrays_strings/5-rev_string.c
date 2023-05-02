d74fce165e530e7ca02e21253be89dbd104c61fa
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-02 10:09:24 -0700

    Print correct then reverse string on new line

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - Write a function that reverses a string
+ * @s: string that will be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, x;
+
+	x = 0;
+
+	for (i = 0; i < 0; i++)
+	{
+		printf("%s\n", s);
+	}
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	for (i = x - 1; i >= 0; i--)
+	{
+		printf("%s\n", s);
+	}
+}
