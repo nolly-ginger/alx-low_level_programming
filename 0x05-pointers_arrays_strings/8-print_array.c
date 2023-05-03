44c0f596bd134a2192d3d5c47844bf29e843198b
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-02 17:05:29 -0700

    Print the n elements in a ttring followed by a line

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - Print n integers from an array followed by a new line
+ * @n: number of elements within the array
+ * @a: the value of the array
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+	}
+	printf("\n");
+}
