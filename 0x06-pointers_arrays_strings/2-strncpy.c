ec1a69b164156b1d290894dd1ba10fc4f0d6bcf3
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-04 08:13:26 -0700

    Copy the two string on value of n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strncpy - entry point of the copying of two string
+ * @dest: strin 1
+ * @src: sting 2
+ * @n: nth value to be copied
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
+
