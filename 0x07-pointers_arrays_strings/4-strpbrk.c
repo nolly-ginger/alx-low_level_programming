060591add6c363e2d492f7eb149f10ee22c69b23
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-08 14:27:31 -0700

    Search for set of bytes in string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - search a string for a set of bytes
+ * @s: bytes of string
+ * @accept: the string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
