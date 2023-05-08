e107a60ef056f3bc572ab96064ddeab37757b30c
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-08 14:16:41 -0700

    Print length of the prefix string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * int _strspn - entry point to get lenth of prefix string
+ * @s: the length of the string
+ * @accept: the string
+ *
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				i++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (i);
+			}
+		}
+		s++;
+	}
+	return (i);
+}
