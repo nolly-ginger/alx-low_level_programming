eb148dd9f087e7d742dc48b81474654e41e8ac95
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-10 05:48:18 -0700

    Print value of x to power of y as given

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,7 +6,7 @@
  * @x: teh number beng returned
  * @y: the power of the number being returned
  *
- * Return: the value of x power by y
+ * Return: Always 0(Success)
  */
 
 int _pow_recursion(int x, int y)
@@ -20,5 +20,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	else
-		return (_pow_recursion(x, y));
+		return (_pow_recursion(x, y - 1) * x);
 }
