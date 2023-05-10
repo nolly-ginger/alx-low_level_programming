d5e519f95b0e733b2ff9b633d41f444195d039d8
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-10 04:18:22 -0700

    Print the string in revrse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print a string in reverse
+ * @s: the string
+ *
+ * Retrun: null
+ */
+
+void _print_rev_recursion(char *s)
+{
+	puts(s);
+	--s;
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,6 @@
 
 void _puts_recursion(char *s);
 int _puthar(char c);
+void _print_rev_recursion(char *s);
 
 #endif
