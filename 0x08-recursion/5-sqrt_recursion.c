7fe38abbb36d4fcefac562ac6d11f458c3b27a38
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-10 06:06:35 -0700

    Print the natual root of given number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - Return natural square root of given number
+ * @n: given number
+ *
+ * Return: Alwas 0(Success)
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return (_sqrt_recursion(n - 1));
+}
