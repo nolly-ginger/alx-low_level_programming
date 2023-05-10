1606417725bee0aff5c2eeb60ba632b93972c548
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-10 03:49:52 -0700

    Print a string with a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - write a string followed by a new line
+ * @s: the string
+ *
+ * Return: null
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - entry point for char c
+ * @c: the character string
+ *
+ * Return: 1 at success, else -1 on error
+ */
+
+int _puthar(char c)
+{
+	return (write(1, &c, 1));
+}
