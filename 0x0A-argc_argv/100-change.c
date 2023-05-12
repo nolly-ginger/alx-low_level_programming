0785c63529db1d41572f389e6d0d917fcd9db834
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-12 10:40:17 -0700

    Print out the smallest num of change for money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entr point to function printing min number of coins
+ * @argc: the size of inputs
+ * @argv: array size arguments
+ *
+ * Return: 0 on success, 1 at error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, tot;
+	int coin[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	tot = 0;
+
+	if (i < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && i >= 0; j++)
+	{
+		while (i > coin[j])
+		{
+			tot++;
+			i -= coin[j];
+		}
+	}
+
+	printf("%d\n", tot);
+	return (0);
+}
