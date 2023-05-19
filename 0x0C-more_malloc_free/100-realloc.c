7fbfe46c26027943fb070978a07269248a91804f
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-19 06:50:28 -0700

    Realloate memory from new to old block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,20 +7,22 @@
  * @old_size: size in bytes for ptr
  * @new_size: new size in byte for ptr
  *
- * Return: void
+ * Return: ptr
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-		ptr = malloc(new_size);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
 
 	free(ptr);
 	ptr = malloc(new_size);
