1a130daabe2effd6d44f61228c049fda6786e4f0
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-22 06:56:30 -0700

    Create new type from struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,12 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
+/**
+ * dog_t - new type from struct dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
