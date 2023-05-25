a9f8a47075bbac680a37f9a2e550448ddbcf63b4
Noluthando <lebohangmashiqa7@gmail.com>
2023-05-25 13:23:35 -0700

    print anything given

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Print anything given
+ * @...: infinite variables
+ * @format: list of variables possible to be printed
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *c, *pep = "";
+	va_list j;
+
+	va_start(j, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", pep, va_arg(j, int));
+					break;
+				case 'i':
+					printf("%s%d", pep, va_arg(j, int));
+					break;
+				case 'f':
+					printf("%s%f", pep, va_arg(j, double));
+					break;
+				case 's':
+					c = va_arg(j, char *);
+					if (!c)
+						printf("(nil)");
+					printf("%s%s", pep, c);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			pep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(j);
+}
