7ed04a0fec9deb11508fc1931949a8d37b982c0a
Noluthando <lebohangmashiqa7@gmail.com>
2023-06-02 01:16:51 -0700

    Print the elements within list_t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
