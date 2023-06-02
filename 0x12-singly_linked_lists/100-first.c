e7313d4a0f480a968e8626a03fe0549e09bf0a73
Noluthando <lebohangmashiqa7@gmail.com>
2023-06-02 14:13:52 -0700

    Print the following string

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@
 
 void sentence(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
