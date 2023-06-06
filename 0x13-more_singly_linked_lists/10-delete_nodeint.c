216c7e000fd93401be807fe94bd784b7373613ff
Noluthando <lebohangmashiqa7@gmail.com>
2023-06-06 08:49:11 -0700

    Delete nodes from the index of the list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
-		if (!first && !(first->next))
+		if (!first || !(first->next))
 		{
 			return (-1);
 		}
