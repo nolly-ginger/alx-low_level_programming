79fb41a84bfbd2572b133cd242c2459020eda0c4
Noluthando <lebohangmashiqa7@gmail.com>
2023-06-06 12:20:36 -0700

    Print the listint_t list linked

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,50 @@
 #include <stdlib.h>
 
 /**
+ * linked_list - free the list
+ * @head: the start of the list
+ *
+ * Return: freed llists
+ */
+
+size_t linked_list(const listint_t *head)
+{
+	const listint_t *i, *j;
+	size_t num = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	i = head->next;
+	j = (head->next)->next;
+
+	while (j)
+	{
+		if (i == j)
+		{
+			i = head;
+			while (i != j)
+			{
+				num++;
+				i = i->next;
+				j = j->next;
+			}
+
+			i = i->next;
+			while (i != j)
+			{
+				num++;
+				i = i->next;
+			}
+			return (num);
+		}
+		i = i->next;
+		j = (j->next)->next;
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - Print the listint_t list liked
  * @head: the beginning of the list
  *
@@ -11,27 +55,27 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *link;
-	size_t num = 0;
+	size_t ext, num = 0;
 
-	if (head == NULL)
-		exit(98);
+	ext = linked_list(head);
 
-	link = head;
-
-	while (link != NULL)
+	if (ext == 0)
 	{
-		printf("%d\n", link->n);
-		num++;
-
-		link = link->next;
-
-		if (link == head)
+		for (; head != NULL; ext++)
 		{
-			printf("-> [%p] %d\n", (void *)link, link->n);
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
+	else
+	{
+		for (num = 0; num < ext; num++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
 
-	return (num);
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (ext);
 }
