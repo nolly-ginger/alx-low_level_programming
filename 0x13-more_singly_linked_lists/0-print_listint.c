5081a816d37045985f9ff6e636644f2ce8c5491f
Noluthando <lebohangmashiqa7@gmail.com>
2023-06-06 06:16:05 -0700

    Print all the elements within the singly linked list provided

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
-	while (h == NULL)
+	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		num++;
 	}
