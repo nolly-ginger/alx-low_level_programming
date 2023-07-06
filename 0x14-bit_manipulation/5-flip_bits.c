b0d2bd3ad9d39685e2848a7296b0612d7039ec3d
Noluthando <lebohangmashiqa7@gmail.com>
2023-07-06 13:31:23 -0700

    Amoun of bits needed to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of bits needed to flip num
+ * @n: the bit number
+ * @m: numer of bits needed
+ *
+ * Return: number of bits needed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, j = 0;
+	unsigned long int k, extra = n ^ m;
+
+	for (i = 63; i >= 0; i++)
+	{
+		k = extra >> i;
+		if (k & 1)
+			j++;
+	}
+	return (j);
+}
